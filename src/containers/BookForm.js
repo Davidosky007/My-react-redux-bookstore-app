f2137d198ef5f6f682733a01d3ab81aea7e93c10
david bassey <davidosky1@gmail.com>
2021-06-22 16:17:30 +0100

    Add bookform.js to render form for adding books

---
+++ src/containers/BookForm.js
@@ -0,0 +1,58 @@
+/* eslint linebreak-style: ["error", "windows"] */
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../actions';
+
+const BooksForm = ({ createBook }) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const categories = ['Action', 'Horror', 'History', 'Kids', 'Biography', 'Learning', 'Sci-Fi'];
+
+  const updateTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const updateCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createBook({
+      id: Math.floor(Math.random() * 100),
+      title: title.trim(),
+      category,
+    });
+    setCategory('Action');
+    setTitle('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={title}
+          placeholder="Title"
+          onChange={updateTitleChange}
+        />
+        <select value={category} onChange={updateCategoryChange}>
+          {categories.map((cat) => <option value={cat} key={cat}>{cat}</option>)}
+        </select>
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  createBook: (book) => dispatch(createBook(book)),
+});
+BooksForm.propTypes = {
+  createBook: PropTypes.func,
+};
+BooksForm.defaultProps = {
+  createBook: null,
+};
+export default connect(null, mapDispatchToProps)(BooksForm);
