5e6186fb916e8bea73f4dc2580f582fe9b71a03f
david bassey <davidosky1@gmail.com>
2021-06-26 18:03:51 +0100

    Add style for progress circle svg design

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,33 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ProgressRing from './ProgressRing';
 
-const Book = ({ book, handleRemoveBook }) => (
-  <div className="buks">
-    <div>
-      <div className="buk-cat">
-        <span>{book.category}</span>
-      </div>
+const Book = ({ book, handleRemoveBook }) => {
+  const progressVal = Math.floor(Math.random() * 90);
+  return (
+    <div className="buks">
       <div>
-        <h3>{book.title}</h3>
+        <div className="buk-cat">
+          <span>{book.category}</span>
+        </div>
+        <div className="buk-title">
+          <h3>{book.title}</h3>
+        </div>
+        <div>
+          <span className="auth-name">Author Name</span>
+        </div>
+        <span className="remove-sect">
+          <ul>
+            <li>Comments</li>
+            <li>
+              {' '}
+              <button type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
+            </li>
+            <li>Edit</li>
+          </ul>
+        </span>
       </div>
-      <div>
-        <span className="auth-name">Author Name</span>
+      <div className="main-prog">
+        <ProgressRing
+          radius={60}
+          stroke={5}
+          progress={progressVal}
+        />
+        <div className="complete">
+          <h2 className="percent">
+            {progressVal}
+            %
+          </h2>
+          <span className="complete">Completed</span>
+        </div>
       </div>
-      <span className="remove-sect">
-        <ul>
-          <li>Comments</li>
-          <li>
-            {' '}
-            <button type="button" onClick={() => handleRemoveBook(book)}>Remove</button>
-          </li>
-          <li>Edit</li>
-        </ul>
-      </span>
+      <div className="current-chap">
+        <div>
+          <span>CURRENT CHAPTER</span>
+        </div>
+        <div>
+          <span>CHAPTER 17</span>
+        </div>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
 
-  </div>
-
-);
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.shape({
--- src/components/ProgressRing.js
+++ src/components/ProgressRing.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ProgressRing extends Component {
+class ProgressRing extends React.Component {
   constructor(props) {
     super(props);
+
     const { radius, stroke } = this.props;
+
     this.normalizedRadius = radius - stroke * 2;
     this.circumference = this.normalizedRadius * 2 * Math.PI;
   }
@@ -12,13 +14,14 @@ class ProgressRing extends Component {
   render() {
     const { radius, stroke, progress } = this.props;
     const strokeDashoffset = this.circumference - (progress / 100) * this.circumference;
+
     return (
       <svg
         height={radius * 2}
         width={radius * 2}
       >
         <circle
-          stroke="#0290ff"
+          stroke="#3690ff"
           fill="transparent"
           strokeWidth={stroke}
           strokeDasharray={`${this.circumference} ${this.circumference}`}
